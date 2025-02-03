f8bb939c801de446510f39597f95f995a6a55ab4
Conspiciens <fernandezjohann08@gmail.com>
2025-02-02 20:14:18 -0800

    Init code

--- sqlite_database_rust/target/debug/deps/sqlite_database_rust-03eb46044487926e.d
+++
@@ -1,5 +0,0 @@
-/Users/jojo/Documents/sqlite_database_rust/target/debug/deps/sqlite_database_rust-03eb46044487926e: src/main.rs
-
-/Users/jojo/Documents/sqlite_database_rust/target/debug/deps/sqlite_database_rust-03eb46044487926e.d: src/main.rs
-
-src/main.rs:
